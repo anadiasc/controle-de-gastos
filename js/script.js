b734a6637277c45ecf4d1be4cb203bf8cd080d3c
Ana Dias <ana.diascarvalh@hotmail.com>
2022-01-16 13:53:47 -0300

    Modificação JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 const controleGastos = {
     orcamento: 0,
     despesa: 0,
@@ -48,4 +44,28 @@ function atualizarInterface(){
     orcamento.innerText = `+ R$ ${controleGastos.orcamento}`;
     despesas.innerText = `- R$ ${controleGastos.despesa}`;
     saldo.innerText = `+ R$ ${controleGastos.saldo}`;
+}
+
+const listaDespesas = document.querySelector('.containerDespesas');
+
+function adicionarDespesaInterface(nomeDespesa, valorDespesa) {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    const img = document.createElement('img');
+
+    h3.innerText = nomeDespesa;
+    p.innerText = `R$ ${valorDespesa}`;
+
+    img.src = 'img/trash.svg';
+    img.alt = 'Icone lixeira';
+
+    img.addEventListener('click', removerDespesa);
+
+    li.dataset.valor = valorDespesa;
+    li.appendChild(h3);
+    li.appendChild(p);
+    li.appendChild(img);
+
+    listaDespesas.appendChild(li);
 }
